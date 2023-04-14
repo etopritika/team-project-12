aa79ba9c6adc4629500aa5611443670c468dc8ab
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-04-14 22:01:08 +0300

    style-card-movie
    Прописал gap

--- src/js/render-trandFilms.js
+++ src/js/render-trandFilms.js
@@ -16,31 +16,46 @@ function getGenreId(genreId) {
 }
 
 function renderMovies(movies) {
-    movieTrand.innerHTML = movies
-      .map(
-        movie => `
+  movieTrand.innerHTML = movies
+    .map(
+      movie => `
         <li class="movie-card__item">
-          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}">
-            <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" />
+          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
+            movie.id
+          }">
+            <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${
+              movie.poster_path
+            }" alt="${movie.title}" />
           </a>
           <div class="movie-card__title">
-            <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}">
+            <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
+              movie.id
+            }">
               ${movie.title}
             </a>
           </div>
           <div class="movie-card__genre">
-          ${movie.genre_ids.map(genre_id => `<a href="https://www.themoviedb.org/genre/${genre_id}">${getGenreId(genre_id)}</a>`).join(', ')}
+          ${movie.genre_ids
+            .map(
+              genre_id =>
+                `<a href="https://www.themoviedb.org/genre/${genre_id}">${getGenreId(
+                  genre_id
+                )}</a>`
+            )
+            .join(', ')}
         </div>
         <div class="movie-card__release-date">
-          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}?language=en-US">
-            ${movie.release_date.split("-")[0]}
+          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
+            movie.id
+          }?language=en-US">
+            ${movie.release_date.split('-')[0]}
           </a>
         </div>
         </li>
       `
-      )
-      .join('');
-  }
+    )
+    .join('');
+}
 
 apiService
   .fetchTrending()
