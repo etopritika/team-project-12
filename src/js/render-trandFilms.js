22be1f2f2699a13a570858b3ed0c385125302ef8
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-04-14 21:26:56 +0300

    movie-trand-render
    1. Заменил классы на БЭМ
    2. Подкоректировал шаблон(добавил ссылки)
    3. Написал стили для рендера и для карточек.

--- src/js/render-trandFilms.js
+++ src/js/render-trandFilms.js
@@ -1,38 +1,48 @@
-// import ApiService from './api-service.js';
-// import genres from '../data/genres';
+import ApiService from './api-service.js';
+import genres from '../data/genres';
 
-// const apiService = new ApiService();
-// const movieTrand = document.querySelector('.trand-films');
+const apiService = new ApiService();
+const movieTrand = document.querySelector('.movie-card');
 
-// function getGenreId(genreId) {
-//   const genre = genres.genres.find(function (genre) {
-//     return genre.id === genreId;
-//   });
-//   if (genre) {
-//     return genre.name;
-//   } else {
-//     return '';
-//   }
-// }
+function getGenreId(genreId) {
+  const genre = genres.genres.find(function (genre) {
+    return genre.id === genreId;
+  });
+  if (genre) {
+    return genre.name;
+  } else {
+    return '';
+  }
+}
 
-// function renderMovies(movies) {
-//   movieTrand.innerHTML = movies
-//     .map(
-//       movie => `
-//       <div class="movie">
-//         <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${
-//         movie.title
-//       }" />
-//         <div class="title">${movie.title}</div>
-//         <div class="genre">${movie.genre_ids.map(getGenreId).join(', ')}</div>
-//         <div class="release-date">${movie.release_date}</div>
-//       </div>
-//     `
-//     )
-//     .join('');
-// }
+function renderMovies(movies) {
+    movieTrand.innerHTML = movies
+      .map(
+        movie => `
+        <li class="movie-card__item">
+          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}">
+            <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" />
+          </a>
+          <div class="movie-card__title">
+            <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}">
+              ${movie.title}
+            </a>
+          </div>
+          <div class="movie-card__genre">
+          ${movie.genre_ids.map(genre_id => `<a href="https://www.themoviedb.org/genre/${genre_id}">${getGenreId(genre_id)}</a>`).join(', ')}
+        </div>
+        <div class="movie-card__release-date">
+          <a class="movie-card__link" href="https://www.themoviedb.org/movie/${movie.id}?language=en-US">
+            ${movie.release_date.split("-")[0]}
+          </a>
+        </div>
+        </li>
+      `
+      )
+      .join('');
+  }
 
-// apiService
-//   .fetchTrending()
-//   .then(renderMovies)
-//   .catch(error => console.log(error));
+apiService
+  .fetchTrending()
+  .then(renderMovies)
+  .catch(error => console.log(error));
