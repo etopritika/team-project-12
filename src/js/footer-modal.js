ce1c8db28e17c985ffc70cf1ebfdb7b638cdfc80
Anastasiia Buhaienko <anastasiia.buhaienko@gmail.com>
2023-04-13 21:30:23 -0500

    footer-teamModal-jsPart

---
+++ src/js/footer-modal.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openFooterTeamModalLink: document.querySelector("[data-modal-team-open]"),
+      closeFooterTeamModalBtn: document.querySelector("[data-modal-team-close]"),
+      footerTeamModal: document.querySelector("[data-modal-team]"),
+    };
+  
+    refs.openFooterTeamModalLink.addEventListener("click", toggleModal);
+    refs.closeFooterTeamModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.footerTeamModal.classList.toggle("is-hidden");
+    }
+  })();
