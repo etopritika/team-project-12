425db3a6387164c52e85d72b33b093e4f2e41a83
etopritika <dimaprytyka@gmail.com>
2023-04-15 15:38:07 +0300

    init

--- src/js/render-trandFilms.js
+++ src/js/render-trandFilms.js
@@ -2,16 +2,18 @@ import ApiService from './api-service.js';
 import genres from '../data/genres';
 
 const apiService = new ApiService();
-const movieTrand = document.querySelector('.movie-card');
+const movieTrand = document.querySelector('.movie-cards');
+let movieParent = document.querySelector(".movie-cards");
 
-movieTrand.addEventListener("click", openModal);
-function openModal(e) {
-  console.log("Target: ",e.target.nodeName);
-  if(e.target.nodeName !== "IMG" && e.target.nodeName !== "H2" && e.target.nodeName !== "P"){
-    return;
+//Прослуховувач подій
+movieParent.onclick = (e) => {
+  let currentCard = e.target.closest("li");
+  if(currentCard) {
+     console.log("Click");
+     //Open modal here
   }
-  console.log(e.target);
 }
+
 
 function getGenreId(genreId) {
   const genre = genres.genres.find(function (genre) {
@@ -23,48 +25,6 @@ function getGenreId(genreId) {
     return '';
   }
 }
-
-// function renderMovies(movies) {
-//   movieTrand.innerHTML = movies
-//     .map(
-//       movie => `
-//         <li class="movie-card__item">
-//           <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
-//             movie.id
-//           }">
-//             <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${
-//               movie.poster_path
-//             }" alt="${movie.title}" />
-//           </a>
-//           <h2 class="movie-card__title">
-//             <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
-//               movie.id
-//             }">
-//               ${movie.title}
-//             </a>
-//           </h2>
-//           <p class="movie-card__genre">
-//           ${movie.genre_ids
-//             .map(
-//               genre_id =>
-//                 `<a href="https://www.themoviedb.org/genre/${genre_id}">${getGenreId(
-//                   genre_id
-//                 )}</a>`
-//             )
-//             .join(', ')}
-//         </p>
-//         <p class="movie-card__release-date">
-//           <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
-//             movie.id
-//           }?language=en-US">
-//             ${movie.release_date.split('-')[0]}
-//           </a>
-//         </p>
-//         </li>
-//       `
-//     )
-//     .join('');
-// }
 
 function renderMovies(movies) {
   movieTrand.innerHTML = movies
