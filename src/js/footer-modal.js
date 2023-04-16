f98e5f3ec4c15922e2a9b4b657f940883dd19e08
Anastasiia Buhaienko <anastasiia.buhaienko@gmail.com>
2023-04-16 17:21:07 -0500

    fix modal team

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,15 +1,22 @@
-// (() => {
-//     const refs = {
-//       openFooterTeamModalLink: document.querySelector("[data-modal-team-open]"),
-//       closeFooterTeamModalBtn: document.querySelector("[data-modal-team-close]"),
-//       footerTeamModal: document.querySelector("[data-modal-team]"),
-//     };
+(() => {
+    const refs = {
+      openFooterTeamModalLink: document.querySelector("[data-modal-team-open]"),
+      closeFooterTeamModalBtn: document.querySelector("[data-modal-team-close]"),
+      footerTeamModal: document.querySelector("[data-modal-team]"),
+      
+      closeFooterTeamModalEsc: document.addEventListener("keydown", function (event) {
+        if(event.keyCode === 27) {
+        toggleModal(event);
+        };
+      }),      
+    };
   
-//     refs.openFooterTeamModalLink.addEventListener("click", toggleModal);
-//     refs.closeFooterTeamModalBtn.addEventListener("click", toggleModal);
+    refs.openFooterTeamModalLink.addEventListener("click", toggleModal);
+    refs.closeFooterTeamModalBtn.addEventListener("click", toggleModal);
+
   
-//     function toggleModal(event) {
-//       event.preventDefault();
-//       refs.footerTeamModal.classList.toggle("is-hidden");
-//     }
-//   })();
+    function toggleModal(event) {
+      event.preventDefault();
+      refs.footerTeamModal.classList.toggle("is-hidden-backdrop-team");
+    }
+  })();
