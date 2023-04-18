5d8dfd2c1b4e839530b51e03cbbfbf8c35636225
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-04-18 14:07:02 +0300

    header-correct-process
    1. Чуть поменял разметку в header.htm
    2. Перебрал header.scss
    3. Добавил другой фон к библиотеке
    4. Подправил размеры логотипа
    5. Прописал логику в header-library_buttons.js. Кнопоки прячуться и добаляються. Поиск тоже.
    6. Добавил ховеры на все елементы.

--- src/js/header-library_buttons.js
+++ src/js/header-library_buttons.js
@@ -1,37 +1,46 @@
 // кнопки основної сторінки
 const libraryButton = document.querySelector('.library_button');
 const homeButton = document.querySelector('.home_button');
-const logoArea = document.querySelector('.logo')
+// const logoArea = document.querySelector('.logo');
 const form = document.querySelector('.search-form');
+const headerOrange = document.querySelector('.header');
+
+// ДОБАВИЛ НА КНОПУ ПОЯВЛЕНИЕ КНОПОК БИБЛИОТЕКИ
+const buttomLibrary = document.querySelector('.headermenu__buttons');
 
 // блок з кнопками бібліотеки
-const divLibraryButtons = document.querySelector('.library__buttons')
+const divLibraryButtons = document.querySelector('.library__buttons');
 
 // відкриття бібліотеки
 libraryButton.addEventListener('click', clickLibraryButton);
 
 // вікриття головної сторінки
-// homeButton.addEventListener('click', showMainPage);
+homeButton.addEventListener('click', showMainPage);
 // logoArea.addEventListener('click', showMainPage);
 
 // функція для відкриття бібліотеки
-function clickLibraryButton(event) {
-    // для зміни кольору фону
-    libraryButton.classList.add('is_active');
-    homeButton.classList.remove('is_active');
-    // для появи кнопок на екрані
-    divLibraryButtons.classList.remove('is_hiden');
-    // приховання форми
-    form.classList.add('is_hiden')
-};
+function clickLibraryButton() {
+  // для зміни кольору фону
+  libraryButton.classList.add('is_active');
+  homeButton.classList.remove('is_active');
+  // для появи кнопок на екрані
+  divLibraryButtons.classList.remove('is_hiden');
+  // приховання форми
+  form.classList.add('is_hidden');
+  buttomLibrary.classList.add('is_activity');
+  headerOrange.classList.add('header-orange');
+}
 
 // фунція для яповернення розмітки на головній сторінці
-function showMainPage(event) {
-    // для зміни кольору фону
-    libraryButton.classList.remove('is_active');
-    homeButton.classList.add('is_active');  
-    // для приховання кнопок бібліотеки на екрані
-    divLibraryButtons.classList.add('is_hiden');
-    // поява форми
-    form.classList.remove('is_hiden')    
-};
+function showMainPage() {
+  // для зміни кольору фону
+  libraryButton.classList.remove('is_active');
+  homeButton.classList.add('is_active');
+  // для приховання кнопок бібліотеки на екрані
+  divLibraryButtons.classList.add('is_hiden');
+  // поява форми
+  form.classList.remove('is_hiden');
+  buttomLibrary.classList.remove('is_activity');
+  form.classList.remove('is_hidden');
+  headerOrange.classList.remove('header-orange');
+}
