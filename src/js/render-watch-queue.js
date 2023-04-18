ca6af764d0b5eccb3e15f43ca175dad14d6f340c
etopritika <dimaprytyka@gmail.com>
2023-04-18 13:14:17 +0300

    init

--- src/js/render-watch-queue.js
+++ src/js/render-watch-queue.js
@@ -52,7 +52,7 @@ function renderMovie(movies) {
             </h2>
             <div class="movie-card__info">
             <p class="movie-card__genre">
-            
+              ${movie.genres.map(genre => genre)}
           </p>
           <p class="movie-card__release-date">
             <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
