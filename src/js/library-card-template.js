8a674c51fc9b14d53d4bc63b4d5651127ae97d20
etopritika <dimaprytyka@gmail.com>
2023-04-19 12:56:56 +0300

    init

---
+++ src/js/library-card-template.js
@@ -0,0 +1,32 @@
+import refs from './refs';
+
+export default function renderMovie(movies) {
+    // console.log(movies);
+   refs.movieTrand.innerHTML = movies
+      .map(
+        movie => `
+          <li class="movie-card__item" data-film-id="${movie.id}">
+              <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${
+                movie.poster_path
+              }" alt="${movie.title}" />
+              <h2 class="movie-card__title">
+                ${movie.title}
+              </h2>
+              <div class="movie-card__info">
+              <p class="movie-card__genre">
+                ${movie.genres.map(genre => genre)}
+            </p>
+            <p class="movie-card__release-date">
+              <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
+                movie.id
+              }?language=en-US">
+              ${movie.release_date.split('-')[0]}
+              </a>
+            </p>  
+              </div>
+              
+          </li>
+        `
+      )
+      .join('');
+  }
--- src/js/render-watch-queue.js
+++ src/js/render-watch-queue.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 import refs from './refs';
-
+import renderMovie from "./library-card-template";
 
 function renderWatchedMovies() {
   const parsedLocalStorageData = JSON.parse(localStorage.getItem('watched-movies'));
@@ -40,36 +40,36 @@ function renderQueueMovies() {
   }
 }
 
-function renderMovie(movies) {
-  // console.log(movies);
- refs.movieTrand.innerHTML = movies
-    .map(
-      movie => `
-        <li class="movie-card__item" data-film-id="${movie.id}">
-            <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${
-              movie.poster_path
-            }" alt="${movie.title}" />
-            <h2 class="movie-card__title">
-              ${movie.title}
-            </h2>
-            <div class="movie-card__info">
-            <p class="movie-card__genre">
-              ${movie.genres.map(genre => genre)}
-          </p>
-          <p class="movie-card__release-date">
-            <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
-              movie.id
-            }?language=en-US">
-            ${movie.release_date.split('-')[0]}
-            </a>
-          </p>  
-            </div>
+// function renderMovie(movies) {
+//   // console.log(movies);
+//  refs.movieTrand.innerHTML = movies
+//     .map(
+//       movie => `
+//         <li class="movie-card__item" data-film-id="${movie.id}">
+//             <img class="movie-card__image" src="https://image.tmdb.org/t/p/w500/${
+//               movie.poster_path
+//             }" alt="${movie.title}" />
+//             <h2 class="movie-card__title">
+//               ${movie.title}
+//             </h2>
+//             <div class="movie-card__info">
+//             <p class="movie-card__genre">
+//               ${movie.genres.map(genre => genre)}
+//           </p>
+//           <p class="movie-card__release-date">
+//             <a class="movie-card__link" href="https://www.themoviedb.org/movie/${
+//               movie.id
+//             }?language=en-US">
+//             ${movie.release_date.split('-')[0]}
+//             </a>
+//           </p>  
+//             </div>
             
-        </li>
-      `
-    )
-    .join('');
-}
+//         </li>
+//       `
+//     )
+//     .join('');
+// }
 
 refs.btnWatched.addEventListener('click', renderWatchedMovies);
 refs.btnQueue.addEventListener('click', renderQueueMovies);
